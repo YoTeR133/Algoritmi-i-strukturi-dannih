ff8fdeb661fd30373d10554de089763bd46d765c
YoTeR133 <78316294+YoTeR133@users.noreply.github.com>
2024-11-19 16:19:42 +0500

    Update interface_impl.py

--- Лабораторные/Лабораторная 3/interface_impl.py
+++ Лабораторные/Лабораторная 3/interface_impl.py
@@ -4,6 +4,8 @@ class UnorderedArray(ArrayInterface):
     def __init__(self, max_size):
         self.elements = []
         self.max_size = max_size
+        self.min_value = None
+        self.max_value = None
 
     def find(self, value):
         return value in self.elements
@@ -11,31 +13,30 @@ class UnorderedArray(ArrayInterface):
     def insert(self, value):
         if len(self.elements) < self.max_size:
             self.elements.append(value)
+            if self.min_value is None or value < self.min_value:
+                self.min_value = value
+            if self.max_value is None or value > self.max_value:
+                self.max_value = value
 
     def delete(self, value):
         if value in self.elements:
             self.elements.remove(value)
+            if value == self.min_value or value == self.max_value:
+                if self.elements:
+                    self.min_value = min(self.elements)
+                    self.max_value = max(self.elements)
+                else:
+                    self.min_value = None
+                    self.max_value = None
 
     def get_elements(self):
         return self.elements
 
     def get_min(self):
-        if not self.elements:
-            return None
-        min_value = self.elements[0]
-        for value in self.elements:
-            if value < min_value:
-                min_value = value
-        return min_value
+        return self.min_value
 
     def get_max(self):
-        if not self.elements:
-            return None
-        max_value = self.elements[0]
-        for value in self.elements:
-            if value > max_value:
-                max_value = value
-        return max_value
+        return self.max_value
 
 class OrderedArray(ArrayInterface):
     def __init__(self, max_size):
