c9c666f3e577c34a742c01b1082a4812cc93c31d
YoTeR133 <78316294+YoTeR133@users.noreply.github.com>
2025-04-21 15:53:16 +0500

    Update graphi.py

--- Лабораторные/Второй семестр/Лабораторная 6/graphi.py
+++ Лабораторные/Второй семестр/Лабораторная 6/graphi.py
@@ -1,19 +1,18 @@
-def sozdat_graf():
-    return {}
+class Graf:
+    def __init__(self):
+        self.smezhnost = {}
 
-def dobavit_vershina(graf, vershina):
-    if vershina not in graf:
-        graf[vershina] = []
+    def dobavit_vershinu(self, vershina):
+        if vershina not in self.smezhnost:
+            self.smezhnost[vershina] = []
 
-def dobavit_rebro(graf, otkuda, kuda):
-    if otkuda not in graf:
-        dobavit_vershina(graf, otkuda)
-    if kuda not in graf:
-        dobavit_vershina(graf, kuda)
-    if kuda not in graf[otkuda]:  
-        graf[otkuda].append(kuda)
+    def dobavit_rebro(self, otkuda, kuda):
+        self.dobavit_vershinu(otkuda)
+        self.dobavit_vershinu(kuda)
+        if kuda not in self.smezhnost[otkuda]:
+            self.smezhnost[otkuda].append(kuda)
 
-def pokazat_graf(graf):
-    for vershina in graf:
-        sosedi = ", ".join(graf[vershina])
-        print(f"{vershina} → {sosedi}")
+    def pokazat_graf(self):
+        for vershina in self.smezhnost:
+            sosedi = ", ".join(self.smezhnost[vershina])
+            print(f"{vershina} → {sosedi}")
